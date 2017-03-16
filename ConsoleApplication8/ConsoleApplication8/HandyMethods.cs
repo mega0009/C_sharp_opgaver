631a0158e51435e1e123cd8943b23a1be1231123
mega0009 <mindra16@student.aau.dk>
2017-03-16 16:18:13 +0100

    Lavet opgave 1 og 2

---
+++ ConsoleApplication8/ConsoleApplication8/HandyMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class HandyMethods
+    {
+        public T Min <T>(params T [] variables) where T : IComparable
+        {
+            if (variables.Length > 0)
+                return variables.Min<T>();
+            else
+                return default(T);
+
+        }
+
+        public T Max<T>(params T[] variables) where T : IComparable
+        {
+            if (variables.Length > 0)
+                return variables.Max<T>();
+            else
+                return default(T);
+
+
+        }
+    }
+}
---
+++ ConsoleApplication8/ConsoleApplication8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HandyMethods numberTest = new HandyMethods();
+            int min = numberTest.Min(250, 5, 10, 35, 2);
+            Console.WriteLine("The minumum number is: " + min);
+            Console.ReadLine();
+            int max = numberTest.Max(350, 450, 320, 10);
+            Console.WriteLine("The maximum number is: " + max);
+            Console.ReadLine();
+            
+        }
+    }
+}
